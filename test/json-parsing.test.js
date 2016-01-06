3797089ae31b6c6f8b2f21bb636b27e14129ab9b
wwayne <wayneup@163.com>
2016-01-06 16:30:33 +0800

    add ttl

--- test/json-parsing.test.js
+++ test/json-parsing.test.js
@@ -25,4 +25,22 @@ describe('json-parsing', function() {
         });
     });
 
+    it('should set ttl successful', function (done) {
+        this.timeout(3000)
+        var m = new Model({name: 'hehe', id: 1});
+        Model.create(m, {ttl: 1}, function (err, mm) {
+            should.not.exist(err);
+            should.exist(mm);
+            setTimeout(function () {
+                Model.findById(1, function (err, mmm) {
+                    should.not.exist(err);
+                    // should.be.empty(mmm)
+                    (mmm).should.be.empty;
+                    done()
+                })
+            }, 2000)
+           
+        })
+    })
+
 });
